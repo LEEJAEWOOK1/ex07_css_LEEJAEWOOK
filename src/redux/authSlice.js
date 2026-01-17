d30e4c0567676f18d2df297454487daa304fdc62
leejaewook <l53941542@gmail.com>
2026-01-18 01:36:53 +0900

    quiz-4 완료

--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -1,9 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-<<<<<<< HEAD
 import { loginThunk, registerThunk } from "../service/authThunk";
-=======
-import { loginThunk } from "../service/authThunk";
->>>>>>> 03d8c8cae655668dbdf0059ee784a38f273c21e6
 import { createLoadingReducers } from "./commonLoadingHandlers";
 const initialState = { isLoggedIn : false, username : null, loading : false, error : null, result : 0 }
 const savedAuth = sessionStorage.getItem("auth")
@@ -23,22 +19,12 @@ const authSlice = createSlice({
     },
     extraReducers : (builder) => {
         builder
-<<<<<<< HEAD
-=======
-        /*
-        .addCase(loginThunk.pending,(state) => {
-            state.loading = true;
-            state.error = null;
-        })
-        */
->>>>>>> 03d8c8cae655668dbdf0059ee784a38f273c21e6
         .addCase(loginThunk.fulfilled,(state, action) => {
             //console.log("fulfilled action : ", action)
             state.result = action.payload.result
             state.loading = false;
             state.error = null;
         })
-<<<<<<< HEAD
         createLoadingReducers(builder, loginThunk);
         
         builder
@@ -49,15 +35,6 @@ const authSlice = createSlice({
             state.error = null;
         })
         createLoadingReducers(builder, registerThunk);
-=======
-        /*
-        .addCase(loginThunk.rejected,(state,action) =>{
-            state.loading = false;
-            state.error = action.error.message;
-        })
-        */
-       createLoadingReducers(builder, loginThunk)
->>>>>>> 03d8c8cae655668dbdf0059ee784a38f273c21e6
     }
 });
 export const {login, logout} = authSlice.actions
